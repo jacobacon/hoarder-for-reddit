011d876cac0f964c139f5dbf5f452d8d826fdc6e
Jacob Beneski <jacobbeneski@gmail.com>
2017-06-07 22:41:58 -0500

    Post.js

--- Post.js
+++ Post.js
@@ -1,3 +1,27 @@
 /**
  * Created by jacob on 6/7/2017.
  */
+
+var Post = class Post{
+
+
+    constructor(title, url){
+        this.title = title;
+        this.url = url;
+    };
+
+
+    getPost(){
+      return (this.title + " "+ this.url);
+    };
+
+
+
+
+
+
+};
+
+module.exports = Post;
+
+
