8709e2437495109d6b1271c245836d2191031a2b
Jacob Beneski <jacobbeneski@gmail.com>
2017-08-20 00:13:19 -0500

    Fix html output

--- filehelper.js
+++ filehelper.js
@@ -13,9 +13,7 @@ exports.buildArray = function (input)
     for (let i = 0; i < input.length; i++) {
         output.push(input[i]);
     }
-    console.log("Here is the output of the array");
-    console.log(output);
-}
+};
 
 
 //Writes the file.
@@ -77,15 +75,18 @@ exports.writeFile = function (location, name, format) {
 function builtHtml() {
     let outputString = "";
 
+
+
+    outputString += "<style type='text/css'>.tg  {border-collapse:collapse;border-spacing:0;margin:0px auto;}.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}.tg .tg-yw4l{vertical-align:top} </style>";
     outputString += "<html>";
     outputString += "<table>";
 
 
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < output.length; i+=2){
         outputString += "<tr>";
 
-        outputString += "<th>" + i + "</th>";
-        outputString += "<th>" + i + "</th>";
+        outputString += "<th>" + output[i] + "</th>";
+        outputString += "<th>" + '<a href="' + output[i+1] + '"> ' + output[i+1] + '</a></th>';
 
         outputString += "</tr>";
     }
