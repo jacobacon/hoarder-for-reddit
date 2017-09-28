855d250de87c0eb44295b3f08189881d22eb768b
Jacob Beneski <jacobbeneski@gmail.com>
2017-09-27 21:02:32 -0500

    Start adding GUI

--- app.js
+++ app.js
@@ -7,6 +7,11 @@
 // 3) Filter promise for wanted data
 // 4) Export filtered data to desired format
 
+const {app, BrowserWindow} = require('electron');
+const http = require('http');
+const url = require('url');
+const ipcMain = require('electron').ipcMain;
+const request = require('request');
 const snoowrap = require('snoowrap');
 var argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
@@ -30,11 +35,117 @@ var argv = require('yargs')
 
 
 var config = require('./config');
-var fh = require('./filehelper');
+const fh = require('./filehelper');
 
 var Post = require('./Post.js');
 
-//var post = new Post('hello', 'world');
+
+let mainWindow;
+
+//Setup Reddit OAuth Variable
+//TODO move reddit oauth to external module
+
+const REDDIT_AUTH = 'https://www.reddit.com/api/v1/authorize.compact';
+const CLIENT_ID = 'm1h0ltl6XNjJ3g';
+const RESPONSE_TYPE = 'code';
+let state = 'jacobacon-' + Math.floor(Math.random() * 10000);
+const REDIRECT_URI = 'http://127.0.0.1:3000/auth/reddit/callback';
+const DURATION = 'permanent';
+const SCOPES = ['identity', 'edit', 'history', 'mysubreddits',
+    'privatemessages', 'read','save'];
+
+
+let authURL = REDDIT_AUTH + '?client_id=' + CLIENT_ID + '&response_type=' + RESPONSE_TYPE +
+    '&state=' + state + '&redirect_uri=' + REDIRECT_URI + '&duration=' + DURATION + '&scope=' + SCOPES.join(' ');
+
+let authToken;
+let refreshToken;
+let timeToExpire;
+
+//End Variables
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+       height: 600,
+        width: 800,
+        show: false
+    });
+
+    if(!authToken){
+        //Show Login
+        showLogin();
+    } else if(timeToExpire){
+        //Refresh Token
+    } else{
+        //Show App
+    }
+});
+
+
+
+
+
+
+
+
+
+function showLogin(){
+    mainWindow.loadURL(authURL);
+    mainWindow.show();
+
+
+    //Create a server for callback
+
+    http.createServer(function (req, res) {
+        console.log('Request:' + req.method + ' to ' + req.url);
+        res.writeHead(200, 'OK');
+        res.write('<h1>Recieved callback...</h1>');
+        res.end();
+        handleCallback(req.url);
+    }).listen(3000);
+    console.log('Ready on port 3000');
+
+}
+
+
+
+function handleCallback(resp){
+    console.log('We got: ' + resp);
+
+    if (resp.includes('error')) {
+        console.error('An Error Occured');
+    }
+
+    //console.log('State starts at: ' + resp.indexOf('state') + ' and code starts at: ' + resp.indexOf('code'));
+
+    let returnState = resp.substring(21, 35);
+
+    let authCode = resp.substring(41);
+
+    console.log('State: ' + returnState + ' code: ' + authCode);
+
+    if (state != returnState) {
+        console.error("Returned State Didn't Match Expected. Try to login again");
+
+        //TODO Handle this Problem better
+    } else {
+        getAuthToken().then(function (result) {
+            console.log(result);
+        });
+    }
+
+
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+    mainWindow.show();
+}
+
+
+
+
+
+
+/*
+
 
 
 const reddit = new snoowrap({
@@ -58,7 +169,7 @@ if (!argv.limit) {
     });
 }
 
-
+*/
 function processSaved(saved) {
 
     console.log('Found ', saved.length, 'saved items.');
